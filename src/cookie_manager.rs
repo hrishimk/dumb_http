d01e87ec282444d9fcaeba4da21d62ac9b5d0895
hrishi <hrishikesh.mk1@gmail.com>
2018-08-30 12:37:33 +0530

    Updated cookies

--- src/cookie_manager.rs
+++ src/cookie_manager.rs
@@ -1,27 +1,104 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-pub struct CookieManager {
-    data: HashMap<String, String>,
+use chrono::prelude::*;
+
+use rand::{thread_rng, Rng};
+
+#[derive(Debug)]
+pub struct Cookie<'a> {
+    name: String,
+    value: String,
+    http_only: bool,
+    secure: bool,
+    has_expiry: bool,
+    expiry: DateTime<Utc>,
+    max_age: u32,
+    path: &'a str,
 }
 
-impl CookieManager {
+impl<'a> Cookie<'a> {
+    pub fn with_value(name: String, value: String) -> Self {
+        Cookie {
+            name,
+            value,
+            http_only: true,
+            secure: false,
+            has_expiry: false,
+            expiry: Utc::now(),
+            max_age: 0,
+            path: "/",
+        }
+    }
+
+    pub fn with_name(name: String) -> Self {
+        let value = Self::gen_cookie_val();
+        Self::with_value(name, value)
+    }
+
+    pub fn set_http_only(&'a mut self, http_only: bool) -> &'a mut Self {
+        self.http_only = http_only;
+        self
+    }
+
+    pub fn set_secure(&'a mut self, http_only: bool) -> &'a mut Self {
+        self.secure = http_only;
+        self
+    }
+
+    pub fn set_max_age(&'a mut self, max_age: u32) -> &'a mut Self {
+        self.max_age = max_age;
+        self
+    }
+
+    fn gen_cookie_val() -> String {
+        thread_rng().gen_ascii_chars().take(30).collect::<String>()
+    }
+
+    pub fn get_value(&self) -> &str {
+        self.value.as_str()
+    }
+
+    pub fn generate(&self) -> String {
+        let mut cookie_string = self.name.to_string();
+        //format()
+        cookie_string = cookie_string + "=" + self.value.as_str();
+        cookie_string = cookie_string + "; Path=" + self.path;
+        if self.max_age > 0 {
+            cookie_string = cookie_string + "; Max-Age=" + self.max_age.to_string().as_str();
+        }
+        if self.http_only {
+            cookie_string = cookie_string + "; HttpOnly";
+        }
+        cookie_string
+    }
+}
+
+#[derive(Debug)]
+pub struct CookieManager<T> {
+    data: HashMap<String, T>,
+}
+
+impl<T> CookieManager<T> {
     pub fn new() -> Self {
         Self {
             data: HashMap::new(),
         }
     }
 
-    pub fn get(&self, key: &str)-> Option<&str>{
-        self.get(key)
+    pub fn get(&self, key: &str) -> Option<&T> {
+        self.data.get(key)
     }
 
-    pub fn set(&mut self, key: &str, value: &str){
-        self.data.entry(key.to_string()).or_insert(value.to_string());
+    pub fn get_mut(&mut self, key: &str) -> Option<&mut T> {
+        self.data.get_mut(key)
     }
 
-    pub fn delete(&mut self, key: &str){
+    pub fn set(&mut self, key: &str, value: T) {
+        self.data.entry(key.to_string()).or_insert(value);
+    }
+
+    pub fn delete(&mut self, key: &str) {
         self.data.remove(key);
     }
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
+extern crate chrono;
+extern crate rand;
+extern crate regex;
+
+use regex::Regex;
+
 use std::collections::HashMap;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-//struct HttpRespBuilder {}
+use std::fs;
+use std::io::prelude::*;
 
 pub mod cookie_manager;
-pub use cookie_manager::CookieManager;
+pub use cookie_manager::{Cookie, CookieManager};
 
+#[derive(Debug)]
 pub struct HttpResp<'a> {
     stream: &'a mut TcpStream,
-    status: u8,
-    headers: HashMap<String, String>,
+    status: u16,
+    pub headers: HashMap<String, Vec<String>>,
     body: Vec<u8>,
 }
 
@@ -30,18 +38,37 @@ impl<'a> HttpResp<'a> {
     }
 
     pub fn set_header(&mut self, key: String, value: String) {
-        self.headers.entry(key).or_insert(value);
+        //self.headers.entry(key).or_insert(value);
+
+        if self.headers.contains_key(&key) {
+            self.headers.get_mut(&key).unwrap().push(value);
+        } else {
+            self.headers.insert(key, vec![value]);
+        }
+    }
+
+    pub fn set_status(&mut self, status: u16) {
+        self.status = status;
+    }
+
+    pub fn redirect(&mut self, page: &str) {
+        self.status = 302;
+        self.set_header("Location".to_string(), page.to_string());
     }
 
     pub fn write(&mut self) {
         let resp = b"HTTP/1.1 200 OK\r\n";
-        self.stream.write(resp);
+        self.stream.write(b"HTTP/1.1 ");
+        self.stream.write(self.status.to_string().as_bytes());
+        self.stream.write(b" OK\r\n");
 
         for (key, value) in &self.headers {
-            self.stream.write(key.as_bytes());
-            self.stream.write(b": ");
-            self.stream.write(value.as_bytes());
-            self.stream.write(b"\r\n");
+            for n in value {
+                self.stream.write(key.as_bytes());
+                self.stream.write(b": ");
+                self.stream.write(n.as_bytes());
+                self.stream.write(b"\r\n");
+            }
         }
 
         self.stream.write(b"\r\n");
@@ -49,6 +76,48 @@ impl<'a> HttpResp<'a> {
         self.stream.write(&self.body);
 
         self.stream.flush();
+    }
+
+    pub fn file(&mut self, path: &str) {
+        let contents = fs::read(path);
+        match contents {
+            Ok(x) => {
+                self.set_header(
+                    "Content-Type".to_string(),
+                    Self::get_mime_type(path).to_string(),
+                );
+                self.set_body(x);
+            }
+            Err(x) => {
+                self.set_header("Content-Type".to_string(), "text/plain".to_string());
+                self.set_body(b"Not Found".to_vec());
+            }
+        }
+    }
+
+    pub fn get_mime_type(path: &str) -> &str {
+        match Self::get_ext(path).as_str() {
+            ".html" => "text/html",
+            ".js" => "text/javascript",
+            ".css" => "text/css",
+            ".jpeg" => "image/jpeg",
+            ".jpg" => "image/jpeg",
+            ".png" => "image/png",
+            ".svg" => "image/svg+xml",
+            _ => "text/html",
+        }
+    }
+
+    pub fn get_ext(path: &str) -> String {
+        let re = Regex::new(r"(\.[0-9a-z]+$)").unwrap();
+        match re.captures(path) {
+            Some(ext) => return ext[0].to_string(),
+            None => return "".to_string(),
+        }
+    }
+
+    pub fn cookie(&mut self, value: String) {
+        self.set_header("Set-Cookie".to_string(), value);
     }
 }
 
